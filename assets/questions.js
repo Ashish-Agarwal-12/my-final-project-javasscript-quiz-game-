37e5e6d989c7c8f0c0d4dba19364512637a47cac
Ashish-Agarwal-12 <agarwalasish567@gmail.com>
2020-11-30 13:36:06 +0530

    final work

---
+++ assets/questions.js
@@ -0,0 +1,114 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "The Shortcut for making a text Bold is",
+    answer: "Ctrl + B",
+    options: [
+      "Ctrl + B",
+      "Ctrl + I",
+      "Ctrl + /",
+      "Ctrl + U"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does JSON Stands For?",
+    answer: "JavaScript Object Notation",
+    options: [
+      "Java Standard Output Network",
+      "JavaScript Object Notation",
+      "JavaScript Output Name",
+      "Java Source Open Network"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+   {
+    numb: 6,
+    question: "What is the use of Ctrl + 2",
+    answer: "Double Spacing",
+    options: [
+      "Single Spacing",
+      "Increase Font Size",
+      "Double Spacing",
+      "Number Lock"
+    ]
+  },
+  {
+    numb: 7,
+    question: "All are text-align shortcuts, except...",
+    answer: "Ctrl + I ",
+    options: [
+      "Ctrl + I ",
+      "Ctrl + E ",
+      "Ctrl + R ",
+      "Ctrl + L "
+    ]
+  },
+  {
+    numb: 8,
+    question: "To Close A Document",
+    answer: "Ctrl + W ",
+    options: [
+      "Ctrl + C ",
+      "Ctrl + End ",
+      "Ctrl + S ",
+      "Ctrl + W "
+    ]
+  },
+  {
+    numb: 9,
+    question: "What does Ctrl + Z is used for",
+    answer: "Undo",
+    options: [
+      "Undo",
+      "Delete",
+      "Redo",
+      "Zip Drive"
+    ]
+  },
+  {
+    numb: 10,
+    question: "F2 key use to change the ________ of file/folder.",
+    answer: "Name",
+    options: [
+      "Colour",
+      "Size",
+      "Style",
+      "Name"
+    ]
+  },
+
+];
